bdc0012070187d4d61a58a2af11d0143984fa553
iomonad <iomonad@riseup.net>
2025-12-01 13:58:25 +0100

    fix[day01] header description

--- notebooks/day01.clj
+++ notebooks/day01.clj
@@ -1,7 +1,7 @@
 ^{:nextjournal.clerk/visibility #{:hide-ns}}
 (ns day01
   {:title "Day01"
-   :description "preview"
+   :description "Day 1: Secret Entrance"
    :path "notebooks/day01"
    :preview "https://images.unsplash.com/photo-1553293377-e51189852c0b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMyfHxzYWZlJTIwbG9ja3xlbnwwfHwwfHx8MA%3D%3D"}
   (:require [clojure.java.io :as io]
